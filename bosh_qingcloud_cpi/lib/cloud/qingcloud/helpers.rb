088bbe8e1fcead22144776b695bf367a3307c094
lirenke <lvhancy@163.com>
2014-06-23 19:45:48 +0800

    更新wait resource功能

--- bosh_qingcloud_cpi/lib/cloud/qingcloud/helpers.rb
+++ bosh_qingcloud_cpi/lib/cloud/qingcloud/helpers.rb
@@ -30,57 +30,6 @@ module Bosh::QingCloud
     MAX_RETRIES = 10 # Max number of retries
     DEFAULT_RETRY_TIMEOUT = 3 # Default timeout before retrying a call (in seconds)
 
-    def with_QingCloud
-      retries = 0
-      begin
-        yield
-      rescue "aaaaa"#Excon::Errors::RequestEntityTooLarge => e
-        # If we find a rate limit error, parse message, wait, and retry
-        overlimit = parse_qingcloud_response(e.response, "overLimit", "overLimitFault")
-        unless overlimit.nil? || retries >= MAX_RETRIES
-          task_checkpoint
-          wait_time = overlimit["retryAfter"] || e.response.headers["Retry-After"] || DEFAULT_RETRY_TIMEOUT
-          details = "#{overlimit["message"]} - #{overlimit["details"]}"
-          @logger.debug("OpenStack API Over Limit (#{details}), waiting #{wait_time} seconds before retrying") if @logger
-          sleep(wait_time.to_i)
-          retries += 1
-          retry
-        end
-        cloud_error("OpenStack API Request Entity Too Large error. Check task debug log for details.", e)
-      rescue "=================="#Excon::Errors::BadRequest => e
-        badrequest = parse_qingcloud_response(e.response, "badRequest")
-        details = badrequest.nil? ? "" : " (#{badrequest["message"]})"   
-        cloud_error("OpenStack API Bad Request#{details}. Check task debug log for details.", e)
-      rescue "++++++++++++++"#Excon::Errors::InternalServerError => e
-        unless retries >= MAX_RETRIES
-          retries += 1
-          @logger.debug("OpenStack API Internal Server error, retrying (#{retries})") if @logger
-          sleep(DEFAULT_RETRY_TIMEOUT)
-          retry
-        end
-        cloud_error("OpenStack API Internal Server error. Check task debug log for details.", e)
-      end
-    end
-    
-    ##
-    # Parses and look ups for keys in an OpenStack response 
-    #
-    # @param [Excon::Response] response Response from OpenStack API
-    # @param [Array<String>] keys Keys to look up in response
-    # @return [Hash] Contents at the first key found, or nil if not found
-    def parse_qingcloud_response(response, *keys)
-      unless response.body.empty?
-        begin
-          body = JSON.parse(response.body)
-          key = keys.detect { |k| body.has_key?(k)}
-          return body[key] if key
-        rescue JSON::ParserError
-          # do nothing
-        end
-      end
-      nil
-    end
-
     ##
     # Waits for a resource to be on a target state
     #
