99db5576db841dcb74ad2f08b766753c07949f2f
wjq1221 <wjq1221@gmail.com>
2014-08-15 15:07:04 +0800

    upload release

--- bosh_hwcloud_cpi/lib/cloud/hwcloud/cloud.rb
+++ bosh_hwcloud_cpi/lib/cloud/hwcloud/cloud.rb
@@ -131,14 +131,14 @@ module Bosh::HwCloud
 
         @logger.info("@hwcloudsdk.run_instances server_options  #{server_options}")
         ret = @hwcloudsdk.run_instances(server_options)
-        @logger.info("@hwcloudsdk.run_instances result  #{ret}")
-	#enough time for slow IAAS
-        sleep(600)
+        @logger.info("wjq:@hwcloudsdk.run_instances result  #{ret}")
+        #enough time for slow IAAS
+        sleep(500)
 #        instance_id = ret['instanceId']
 #@logger.info("wjq:instance_id---->#{instance_id}")
         instance_id = ret['runInstancesSet']['runInstancesSet'][0]['instanceId']
 
-#@logger.info("wjq:test_myself--->#{instance_id}")
+@logger.info("wjq:test_myself--->#{instance_id}")
       
         #wait running
         wait_resource(instance_id, "running", method(:get_vm_status))
@@ -156,11 +156,13 @@ module Bosh::HwCloud
         network_configurator.configure(@hwcloudsdk, instance_info)
 
 #server_name--->instance_ip
+        ensure
         settings = initial_agent_settings(instance_ip, agent_id, network_spec, environment,
                                             flavor_has_ephemeral_disk?(instance_type))
+@logger.info("wjqq:settings---->#{settings}")
         @registry.update_settings(instance_ip, settings)
-
-        ensure
+        sleep(60)
+      #  ensure
           return instance_id
         end
       end
@@ -205,6 +207,7 @@ module Bosh::HwCloud
     def get_vm_status(instance_id)
       with_thread_name("get_vm_status(#{instance_id})") do
         options = {'InstanceId[0]'.to_sym => instance_id}
+logger.info("wjq:options--->#{options}")
         instance = @hwcloudsdk.describe_instances(options)
         logger.info("instance info: #{instance}")
 #        logger.info("instance state: #{instance['instancesSet']['instancesSet'][0]['instanceState']['name']}")
@@ -630,7 +633,6 @@ module Bosh::HwCloud
           :HWSAccessKeyId => hwcloud_properties['access_key_id'],
           :Version => hwcloud_properties['version'],
           :SignatureMethod => hwcloud_properties['signature_method'],
-          :SignatureNonce => hwcloud_properties['signature_nonce'],
           :SignatureVersion => hwcloud_properties['signature_version'],
           :RegionName => hwcloud_properties['region_name'],
           :Key => hwcloud_properties['key'],
@@ -675,6 +677,7 @@ module Bosh::HwCloud
         yield settings
         #registry.update_settings(instance_info["instancesSet"]["instancesSet"][0]["instanceName"], settings)
         registry.update_settings(instance_info['instancesSet']['instancesSet'][0]['privateIpAddress'], settings)
+        @logger.info("wjqqq:update_settings:#{settings}")
       else
         @logger.info("Server `#{instance_id}' not found. Skipping.")
       end
